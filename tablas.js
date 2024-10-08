6b7a2af57a6703ce8dd984a3208ff96445babbc0
Wolfseran <alandom96@gmail.com>
2024-10-07 20:35:07 -0500

    Add files via upload

---
+++ tablas.js
@@ -0,0 +1,8 @@
+// Escribir el número para generar la tabla de multiplicar
+let numero = 100; // Cambia este número por el que desees por ejemplo 100
+
+// Generar la tabla de multiplicar del 1 al 10, también probé que se puede aumentar los números que quiera como 20
+console.log(`Tabla de multiplicar del ${numero}:`);
+for (let i = 1; i <= 20; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
